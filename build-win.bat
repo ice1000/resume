4c6b142e13759f80bd497632ae22af7709d49c35
ice1000 <ice1000kotlin@foxmail.com>
2023-08-20 14:55:49 -0400

    Rename file

--- build-win.bat
+++ build-win.bat
@@ -1,8 +1,8 @@
 @echo off
 del *.pdf
 
-set files=resume-zh,resume,resume-elab
-echo Building...
+set files=resume-cn,resume,resume-elab
+echo Generating...
 
 for %%f in (%files%) do (
   (
@@ -11,8 +11,12 @@ for %%f in (%files%) do (
   ) > %%f.typ
 )
 
-echo #runReader(Chinese) >> resume-zh.typ
+echo #runReader(Chinese) >> resume-cn.typ
 echo #runReader(Simplified) >> resume.typ
 echo #runReader(EnglishFull) >> resume-elab.typ
 
-for %%f in (%files%) do typst compile %%f.typ
+for %%f in (%files%) do (
+  echo Building %%f.typ
+  typst compile %%f.typ
+  del %%f.typ
+)
--- resume-zh.typ
+++
@@ -1,3 +0,0 @@
-#import "main.typ": *;
-#show: chicv
-#runReader(Chinese) 
